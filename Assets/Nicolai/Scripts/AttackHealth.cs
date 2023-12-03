a2750dc619e7edc76eef1d64c0f2a7a1ad37e0b4
nico386d <nico386d@gmail.com>
2023-12-03 11:25:51 +0100

    attack method update

---
+++ Assets/Nicolai/Scripts/AttackHealth.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackHealth : MonoBehaviour
+{
+    public void attack(float timer, float attackSpeed, int damages, GameObject target, EnemyController enemyScript, Defence defenceScript)
+    {
+        timer += Time.deltaTime;
+        if (timer >= attackSpeed)
+        {
+            if (target.GetComponent<EnemyController>() == enemyScript)
+            {
+                enemyScript.health -= damages; // Reduce health of EnemyController
+
+                if (defenceScript.health <= 0)
+                {
+                    Destroy(target);
+                }
+            }
+            else if (target.GetComponent<Defence>() == defenceScript)
+            {
+                defenceScript.health -= damages; // Reduce health of Defence
+
+                if (defenceScript.health <= 0)
+                {
+                    Destroy(target);
+                }
+            }
+
+            timer = 0;
+        }
+
+       
+    }
+}
--- Assets/Nicolai/Scripts/EnemyController.cs
+++ Assets/Nicolai/Scripts/EnemyController.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public int health;
-
+    
     public int damage;
     public float attackZone;
     public float attackSpeed;
     public float timer;
-    
-    private GameObject target;
+
+    public AttackHealth attackHealth;
+    public GameObject currentTarget;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,7 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (health <= 0)
         {
             Destroy(gameObject);
@@ -30,10 +32,15 @@ public class EnemyController : MonoBehaviour
         //when we are close enugh we should attack
         if(GetComponent<EnemyNav>().target != null)
         {
+            //used to know what object to damages
+            currentTarget = GetComponent<EnemyNav>().target.gameObject;
+
+
             float distance = Vector3.Distance(transform.position, GetComponent<EnemyNav>().target.position);
             if (distance <= attackZone) {
                 print("true");
                 attack(damage);
+                
             }
         }
         
@@ -45,7 +52,7 @@ public class EnemyController : MonoBehaviour
         timer += Time.deltaTime;
         if (timer >= attackSpeed)
         {
-            GetComponent<EnemyNav>().target.GetComponent<Defence>().health -= damages;
+            currentTarget.GetComponent<Defence>().health -= damages;
             timer = 0;
         }
 
